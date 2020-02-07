95fcdb665de6667b12c00431671c8f058c56c2af
Marius Mercure <zeddmeki@gmail.com>
2020-02-07 16:15:51 +0400

    update ui to v2 , add colors to distinguish status

--- meeting/urls.py
+++ meeting/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from meeting.views import AppointList, AppointCreate
+from meeting.views import AppointList, AppointCreate, AppointUpdate
 
 urlpatterns = [
     path('', AppointList.as_view(), name='meeting-list'),
     path('create/', AppointCreate.as_view(), name='meeting-create'),
+    path('<slug:slug>/edit/', AppointUpdate.as_view(), name='meeting-update'),
 
 ]
