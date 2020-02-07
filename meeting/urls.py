c12483e075c61abf25750a61cbddfba1679acac7
Marius Mercure <zeddmeki@gmail.com>
2020-02-07 15:14:00 +0400

    add jquerymodal to make ajax request forms update ui v1 add assets to designated fill

--- meeting/urls.py
+++ meeting/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from meeting.views import AppointList
+from meeting.views import AppointList, AppointCreate
 
 urlpatterns = [
     path('', AppointList.as_view(), name='meeting-list'),
+    path('create/', AppointCreate.as_view(), name='meeting-create'),
 
 ]
